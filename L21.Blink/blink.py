43801ae00f4416c5aaa5e2dd687cf03b2409752d
k2controls <kkelly@k2controls.net>
2020-09-30 10:09:29 -0400

    udpated blink

--- L21.Blink/blink.py
+++ L21.Blink/blink.py
@@ -1,11 +1,11 @@
-'''pirover blink solution
+'''piRover blink solution
 Keith E. Kelly
 8/24/20
 '''
 import time
 
-'flash for 50 times'
-while true:
+
+while True:
     print("turn the red LED on")
     time.sleep(1)
     print("turn the red LED off")
