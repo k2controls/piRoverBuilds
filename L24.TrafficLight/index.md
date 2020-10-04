4255b54fafbaeddec3fdd91e7224b18ce0695b9b
k2controls <kkelly@k2controls.net>
2020-10-04 13:17:55 -0400

    added traffic light

---
+++ L24.TrafficLight/index.md
@@ -0,0 +1,14 @@
+---
+layout: default
+---
+
+# piRover Builds by K2
+
+## Traffic Light Project
+
+- **Document:** [Traffic Light](traffic_light.pdf){:target="_blank"}
+- **Python:** [traffic_light.py](traffic_light.py){:target="_blank"}
+
+
+
+
---
+++ L24.TrafficLight/traffic_light.py
@@ -0,0 +1,29 @@
+''' Traffic Light Simulation - Student Project
+[your name]
+[date]
+'''
+#import required libraries
+
+#create constants to represent piRover LED pin numbers
+
+#create constant to represent the green, amber,and red timings for NS
+
+#create constant to represent the green, amber,and red timings for EW
+
+#create timing variables for green, amber, and red
+green_delay = 0
+amber_delay = 0
+red_delay = 0 
+
+# Configure GPIO setting
+
+# Set specific pints as output
+
+# Initialize all LEDs to OFF
+
+# Welcome user and prompt for NS or EW simulation
+
+# Check user's direction input. If valid then set light timing variables
+
+# Use a while loop initially to test light cycles
+# Implement for loop as time permits
