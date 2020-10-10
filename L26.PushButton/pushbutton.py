828887a19ca7e0b82f86c21c94c8744c95c51109
k2controls <kkelly@k2controls.net>
2020-10-10 10:05:05 -0400

    updated pushbutton sample

--- L26.PushButton/pushbutton.py
+++ L26.PushButton/pushbutton.py
@@ -1,29 +1,38 @@
-''' Traffic Light Simulation - Student Project
-[your name]
-[date]
+''' Pushbutton Solution
+Keith E. Kelly
+10/4/20
 '''
 #import required libraries
+import RPi.GPIO as GPIO
+import time
 
 #create constants to represent piRover LED pin numbers
-
-#create constant to represent the green, amber,and red timings for NS
-
-#create constant to represent the green, amber,and red timings for EW
-
-#create timing variables for green, amber, and red
-green_delay = 0
-amber_delay = 0
-red_delay = 0 
+RED_PIN = 15
+GREEN_PIN = 13
+BLUE_PIN = 18
+#create constants to represent piRover pushbutton pin number
+PB_PIN = 24
 
 # Configure GPIO setting
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-# Set specific pints as output
+# Set pin LED pins as output
+GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(GREEN_PIN, GPIO.OUT)
+GPIO.setup(BLUE_PIN, GPIO.OUT)
+# Set pushbutton pin as input
+GPIO.setup(PB_PIN, GPIO.IN)
 
-# Initialize all LEDs to OFF
-
-# Welcome user and prompt for NS or EW simulation
-
-# Check user's direction input. If valid then set light timing variables
-
-# Use a while loop initially to test light cycles
-# Implement for loop as time permits
+while True:
+    #get state of pin and update LEDs
+    state = GPIO.input(PB_PIN)
+    if state == True:  #active low
+        GPIO.output(RED_PIN, False)
+        GPIO.output(GREEN_PIN, False)
+        GPIO.output(BLUE_PIN, False)
+    else:
+        GPIO.output(RED_PIN, True)
+        GPIO.output(GREEN_PIN, True)
+        GPIO.output(BLUE_PIN, True)
+    time.sleep(.1)
